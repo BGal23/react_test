349bf9f8a9d3b3b8ef1740aa856020801ad59c11
Bartłomiej Gal <bgal23@bartomiejs-macbook-pro.home>
2023-11-27 20:08:39 +0100

    new id

--- src/name/NameCode.jsx
+++ src/name/NameCode.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import name from "./name.module.css";
 
 const Name = () => {
   const names = ["Kevin", "Mango", "Polly"];
 
-  const table = names.map((name) => <li key={name}>{name}</li>);
+  const table = names.map((name) => <li key={uuidv4()}>{name}</li>);
 
   const [toggle, setToggle] = useState(true);
 
