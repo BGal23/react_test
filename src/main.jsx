2940c0863d57aa61a2b1bc6292a5b889d648c016
Bartłomiej Gal <bgal23@bartomiejs-macbook-pro.home>
2023-12-05 11:24:11 +0100

    duba

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+//import App from "./lesson1/App.jsx";
+import App_2 from "./lesson2/App_2.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <App_2 />
   </React.StrictMode>
 );
